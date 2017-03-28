ccfb6e49d9c621bb0396782e506087809d1ffc14
ListsOfArrays <ListsOfArrays@users.noreply.github.com>
2017-03-28 18:51:55 -0400

    Choice working.

--- app/src/main/java/ucf/cap4104/group17/factorcrap/Choice.java
+++ app/src/main/java/ucf/cap4104/group17/factorcrap/Choice.java
@@ -1,11 +1,18 @@
 package ucf.cap4104.group17.factorcrap;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.security.SecureRandom;
+import java.util.Random;
 
 public class Choice extends AppCompatActivity {
 
@@ -14,6 +21,32 @@ public class Choice extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choice);
 
+        Button longGame = (Button) findViewById(R.id.button5);
+        Button shortGame = (Button) findViewById(R.id.button6);
+
+        longGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Totalcount.class));
+                finish();
+            }
+        });
+        shortGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Totalcount.class));
+                finish();
+            }
+        });
+
+        TextView session = (TextView) findViewById(R.id.textView);
+        SecureRandom random = new SecureRandom();
+        byte[] arr = new byte[10];
+        random.nextBytes(arr);
+        String randomStr = Base64.encodeToString(arr, Base64.URL_SAFE);
+        randomStr = "Session ID: " + randomStr.replaceAll("=", "");
+
+        session.setText(randomStr);
     }
 
 }
