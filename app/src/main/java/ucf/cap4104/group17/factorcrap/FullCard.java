17ff5c095b3f964c345622cacec72a94c6da7f86
ListsOfArrays <ListsOfArrays@users.noreply.github.com>
2017-03-30 18:04:15 -0400

    final grammar

--- app/src/main/java/ucf/cap4104/group17/factorcrap/FullCard.java
+++ app/src/main/java/ucf/cap4104/group17/factorcrap/FullCard.java
@@ -12,9 +12,9 @@ public class FullCard implements CardDescription {
         ArrayList<FullCard> fullCardList = new ArrayList<>(64);
         fullCardList.add(new FullCard("When a male penguin falls in love with female penguin, he searches the entire beach to find the perfect pebble to present to her.", true));
         fullCardList.add(new FullCard("New Zealand will deny people residency visa’s if they have too high of a BMI and there are cases where people have been rejected because of their weight.", true));
-        fullCardList.add(new FullCard("Whenever a pregnant women suffers from organ damage like heart attack, the fetus sends stem cells to the organ helping it to repair.", true));
+        fullCardList.add(new FullCard("Whenever a pregnant woman suffers from organ damage like heart attack, the fetus sends stem cells to the organ helping it to repair.", true));
         fullCardList.add(new FullCard("It is illegal to climb trees in Oshawa, a town in Ontario, Canada.", true));
-        fullCardList.add(new FullCard("Brown eyes are blue underneath, and you can actually get a surgery to turn brown eyes blue.", true));
+        fullCardList.add(new FullCard("Brown eyes are blue underneath, and you can get surgery to turn brown eyes blue.", true));
         fullCardList.add(new FullCard("When you blush, the lining of your stomach also turns red.", true));
         fullCardList.add(new FullCard("A bolt of lightning is six times hotter than the sun.", true));
         fullCardList.add(new FullCard("When a person cries and the first drop of tears come from the right eye, it's happiness. If it's from left eye, it’s pain.", true));
@@ -22,7 +22,7 @@ public class FullCard implements CardDescription {
         fullCardList.add(new FullCard("Having bridesmaids in a wedding wasn’t originally for moral support. They were intended to confuse evil spirits or those who wished to harm the bride.", true));
         fullCardList.add(new FullCard("Mount Everest is pronounced as Eve-rest, not Ever-est , as it is named after George Everest.", true));
         fullCardList.add(new FullCard("All pandas in the world are on loan from China.", true));
-        fullCardList.add(new FullCard("When howling together, no two wolves will howl on the same note, instead, they harmonize to create the illusion that there are more of them than there actually are.", true));
+        fullCardList.add(new FullCard("When howling together, no two wolves will howl on the same note, instead, they harmonize to create the illusion that there are more of them than there are.", true));
         fullCardList.add(new FullCard("Your nose can remember 50,000 different scents.", true));
         fullCardList.add(new FullCard("India’s “Go Air” airline only hires female flight attendants because they are lighter, so they save up to US $500,000 per year in fuel.", true));
         fullCardList.add(new FullCard("At room temperature, the average air molecule travels at the speed of a rifle bullet.", true));
@@ -44,10 +44,10 @@ public class FullCard implements CardDescription {
         fullCardList.add(new FullCard("Melting ice and icebergs make frizzing noise called “bergy seltzer”.", true));
         fullCardList.add(new FullCard("Some tumors can grow hair, bones and teeth.", true));
         fullCardList.add(new FullCard("Taking a quick nap after learning can help strengthen your memory.", true));
-        fullCardList.add(new FullCard("A study found that Having friends from other cultures makes you more creative.", true));
+        fullCardList.add(new FullCard("A study found that having friends from other cultures makes you more creative.", true));
         fullCardList.add(new FullCard("Shark pregnancies last up to 4 years.", true));
         fullCardList.add(new FullCard("A full moon is nine times brighter than a half moon.", true));
-        fullCardList.add(new FullCard("Smiling actually boosts your immune system.", true));
+        fullCardList.add(new FullCard("Smiling boosts your immune system.", true));
         fullCardList.add(new FullCard("Dogs and elephants are the only animals that understand pointing.", true));
         fullCardList.add(new FullCard("The main exporter of Brazil nuts is not Brazil. It’s Bolivia.", true));
         fullCardList.add(new FullCard("At rest, your brain burns one fifth of a calorie per minute.", true));
@@ -58,7 +58,7 @@ public class FullCard implements CardDescription {
         fullCardList.add(new FullCard("You can’t fold a piece of paper in half more than 7 times.", false));
         fullCardList.add(new FullCard("Elephants are the only mammal that can’t jump.", false));
         fullCardList.add(new FullCard("One dog year is equal to seven human years.", false));
-        fullCardList.add(new FullCard("If someone wrongly advertises goods for the wrong price, they have to sell it to you at that price.", false));
+        fullCardList.add(new FullCard("If someone wrongly advertises goods for the wrong price, they must sell it to you at that price.", false));
         fullCardList.add(new FullCard("NASA invented the DustBuster.", false));
         fullCardList.add(new FullCard("Polar Bears are left handed.", false));
         fullCardList.add(new FullCard("No two countries with McDonald’s franchises have ever gone to war.", false));
